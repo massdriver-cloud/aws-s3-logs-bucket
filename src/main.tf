2709fef0b19bbc4de6e789e860c65a6d13b2efb4
Chris Hill <chris@massdriver.cloud>
2023-10-17 21:52:18 -0700

    disable ACL, enforce bucket privacy and variablize force_destroy

--- src/main.tf
+++ src/main.tf
@@ -1,18 +1,19 @@
 resource "aws_s3_bucket" "main" {
   bucket        = var.md_metadata.name_prefix
-  force_destroy = false
+  force_destroy = lookup(var.bucket, "force_destroy", false)
 }
 
-# Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
 resource "aws_s3_bucket_ownership_controls" "main" {
   bucket = aws_s3_bucket.main.id
   rule {
-    object_ownership = "ObjectWriter"
+    # this setting disables ACLs
+    object_ownership = "BucketOwnerEnforced"
   }
 }
 
 resource "aws_s3_bucket_acl" "main" {
   bucket     = aws_s3_bucket.main.id
+  // this setting (combined w/ "BucketOwnerEnforced" above) disables ACLs
   acl        = "private"
   depends_on = [aws_s3_bucket_ownership_controls.main]
 }
@@ -20,9 +21,10 @@ resource "aws_s3_bucket_acl" "main" {
 resource "aws_s3_bucket_public_access_block" "main" {
   bucket = aws_s3_bucket.main.id
 
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls  = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "main" {
