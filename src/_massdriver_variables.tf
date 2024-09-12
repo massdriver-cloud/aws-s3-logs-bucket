f9f518a6e12e27439d166d8d7acb33e40479deeb
Michael Lacore <michael@massdriver.cloud>
2024-09-12 12:15:07 -0700

    Update `operator.mdx` to `operator.md` (#7)

---
+++ src/_massdriver_variables.tf
@@ -0,0 +1,63 @@
+// Auto-generated variable declarations from massdriver.yaml
+variable "aws_authentication" {
+  type = object({
+    data = object({
+      arn         = string
+      external_id = optional(string)
+    })
+    specs = object({
+      aws = optional(object({
+        region = optional(string)
+      }))
+    })
+  })
+}
+variable "bucket" {
+  type = object({
+    customer_managed_key = bool
+    force_destroy        = bool
+    region               = string
+  })
+}
+variable "lifecycle_settings" {
+  type = object({
+    expire = bool
+    transition_rules = list(object({
+      days          = number
+      storage_class = string
+    }))
+    expiration_days = optional(number)
+  })
+}
+variable "md_metadata" {
+  type = object({
+    default_tags = object({
+      managed-by  = string
+      md-manifest = string
+      md-package  = string
+      md-project  = string
+      md-target   = string
+    })
+    deployment = object({
+      id = string
+    })
+    name_prefix = string
+    observability = object({
+      alarm_webhook_url = string
+    })
+    package = object({
+      created_at             = string
+      deployment_enqueued_at = string
+      previous_status        = string
+      updated_at             = string
+    })
+    target = object({
+      contact_email = string
+    })
+  })
+}
+variable "monitoring" {
+  type = object({
+    access_logging = bool
+  })
+}
